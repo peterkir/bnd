5fa6d5eb630a79d2fa874a7b21cacaf6cc5cbcdb
Peter Kirschner <peter@kirschners.de>
2015-06-15 09:19:23 +0200

    Revert "Create only Unix style line endings (LF) for component and metatype generation"
    This reverts commit 3d10d81157e08a836e1f9c5cecd4a3dee58bd2a2.

--- biz.aQute.bndlib/src/aQute/bnd/metatype/TagResource.java
+++ biz.aQute.bndlib/src/aQute/bnd/metatype/TagResource.java
@@ -16,7 +16,7 @@ public class TagResource extends WriteResource {
 	public void write(OutputStream out) throws UnsupportedEncodingException {
 		OutputStreamWriter ow = new OutputStreamWriter(out, "UTF-8");
 		PrintWriter pw = new PrintWriter(ow);
-		pw.print("<?xml version=\"1.0\"?>" + "\n");
+		pw.println("<?xml version=\"1.0\"?>");
 		try {
 			tag.print(0, pw);
 		}
